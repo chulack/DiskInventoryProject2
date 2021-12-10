a3795ec1d23efe30aca948bbb93702e9f8cb3f75
Luke Chulack <70914959+chulack@users.noreply.github.com>
2021-12-10 14:45:28 -0700

    Add files via upload

--- DiskInventory/Models/Borrower.cs
+++ DiskInventory/Models/Borrower.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,8 +14,11 @@ namespace DiskInventory.Models
         }
 
         public int BorrowerId { get; set; }
+        [Required]
         public string Fname { get; set; }
+        [Required]
         public string Lname { get; set; }
+        [Required]
         public string BorrowerPhoneNum { get; set; }
 
         public virtual ICollection<MediaIntersectiontable> MediaIntersectiontables { get; set; }
