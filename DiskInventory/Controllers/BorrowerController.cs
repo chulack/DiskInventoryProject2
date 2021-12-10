319b7fd31462bc2867276440e780fe4dc6afab6b
Luke Chulack <70914959+chulack@users.noreply.github.com>
2021-12-10 14:46:46 -0700

    Add files via upload

--- DiskInventory/Controllers/BorrowerController.cs
+++ DiskInventory/Controllers/BorrowerController.cs
@@ -8,13 +8,14 @@ using DiskInventory.Models;
 /*
     Original Author: Luke Chulack                         
     Date Created:  11/12/2021                                     
-    Version: 1.0                                           
-    Date Last Modified: 11/12/2021                               
+    Version: 2.0                                           
+    Date Last Modified: 12/3/2021                               
     Modified by: Luke Chulack                                          
     Modification log: 
 
            version 1.0 -  11/12/2021  - Built the Borrower Controller which links to the view in the Borrower view folder
- 
+            version 2.0 -  12/3/2021  - added logic foradd, edit and delete of Borrowers data.
+
  */
 
 
@@ -36,5 +37,70 @@ namespace DiskInventory.Controllers
             List<Borrower> borrowers = context.Borrowers.OrderBy(b => b.Lname).ThenBy(b => b.Fname).ToList();
             return View(borrowers);
         }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new Borrower());
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id) // overloaded edit to get Borrower id
+        {
+            ViewBag.Action = "Edit";
+            var borrower = context.Borrowers.Find(id);
+            return View(borrower);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Borrower borrower)
+        {
+            if (ModelState.IsValid)
+            {
+                if (borrower.BorrowerId == 0) // add Borrower
+                {
+                    context.Borrowers.Add(borrower);
+
+                }
+                else // update Borrower
+                {
+                    context.Borrowers.Update(borrower);
+                }
+                context.SaveChanges();
+                return RedirectToAction("Index", "Borrower");
+
+            }
+            else
+            {
+                ViewBag.Action = (borrower.BorrowerId == 0) ? "Add" : "Edit";
+                return View(borrower);
+            }
+
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult Delete(int id) // gets id to delete
+        {
+            var borrower = context.Borrowers.Find(id);
+            return View(borrower);
+        }
+
+        [HttpPost]
+
+        public IActionResult Delete(Borrower borrower) // overload delete
+        {
+            context.Borrowers.Remove(borrower);
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Borrower");
+        }
+
+
     }
 }
---
+++ DiskInventory/Controllers/MediaIntersectiontableController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiskInventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+/*
+    Original Author: Luke Chulack                         
+    Date Created:  12/3/2021                                     
+    Version: 1.0                                           
+    Date Last Modified: 12/3/2021                              
+    Modified by: Luke Chulack                                          
+    Modification log: 
+
+           version 1.0 -  11/12/2021  - Built the MediaIntersectiontable Controller which links to the view in the MediaIntersectiontable view folder
+
+ */
+
+namespace DiskInventory.Controllers
+{
+
+    // media intersection table controller
+    public class MediaIntersectiontableController : Controller
+    {
+
+        private disk_inventorylcContext context { get; set; }
+
+        public MediaIntersectiontableController(disk_inventorylcContext ctx)
+        {
+            context = ctx;
+
+        }
+
+        public IActionResult Index()
+        {
+
+            var mediaintersectiontable = context.MediaIntersectiontables.Include(d => d.Media).OrderBy(d => d.Media.MediaName).Include(b => b.Borrower).ToList();
+
+            return View(mediaintersectiontable);
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Borrowers = context.Borrowers.OrderBy(t => t.Lname).ToList();
+            ViewBag.Media = context.Media.OrderBy(s => s.MediaName).ToList();
+
+            MediaIntersectiontable newMedia = new MediaIntersectiontable();
+
+            newMedia.BorrowedDate = DateTime.Today;
+
+            return View("Edit", newMedia);
+        }
+
+
+        [HttpGet]
+
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Borrowers = context.Borrowers.OrderBy(t => t.Lname).ToList();
+            ViewBag.Media = context.Media.OrderBy(s => s.MediaName).ToList();
+
+            var mediaintersectiontable = context.MediaIntersectiontables.Find(id);
+            return View(mediaintersectiontable);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(MediaIntersectiontable mediaintersectiontable)
+        {
+            if (ModelState.IsValid)
+            {
+                if (mediaintersectiontable.MediaIntersectionId == 0) // add media
+                {
+                    context.MediaIntersectiontables.Add(mediaintersectiontable);
+
+                }
+                else // update media
+                {
+                    context.MediaIntersectiontables.Update(mediaintersectiontable);
+                }
+                context.SaveChanges();
+                return RedirectToAction("Index", "MediaIntersectiontable");
+
+            }
+            else
+            {
+                ViewBag.Action = (mediaintersectiontable.MediaIntersectionId == 0) ? "Add" : "Edit";
+                ViewBag.Borrowers = context.Borrowers.OrderBy(t => t.Lname).ToList();
+                ViewBag.Media = context.Media.OrderBy(s => s.MediaName).ToList();
+                return View(mediaintersectiontable);
+            }
+
+        }
+
+    }
+}
